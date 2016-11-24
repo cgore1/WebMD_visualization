f408053f32bb093d15e83d8520f1ca6680c3ca89
Chinmay Chandrashekhar Gore (Student) <cgore1@sundevils.asu.edu>
2016-11-24 16:10:59 -0700

    timeline

--- timeline.js
+++ timeline.js
@@ -1,101 +1,82 @@
 function loadTimeline(topic)
 {
-	var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+$(document).ready(function() {
+   // month 
+      d3.select("#monthChart").html('');
+        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        ];
 
-var parseDate = d3.time.format("%m").parse;
+        if(!topic)
+          return;
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+        data = [];
+        for(var i=0; i<monthNames.length; i++)
+        {
+           var r = {};
+           // r.date = parseDate(m)
+           r.period = monthNames[i];
+           r.value = monthD[topic][i];
+           if(r.value == undefined)
+           {
+             r.value = 0;
+           }
+           data.push(r);
+        }
+        Morris.Bar({
+          element: 'monthChart',
+          data,
+          xkey: 'period',
+          hideHover: 'auto',
+          barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+          ykeys: ['value', 'sorned'],
+          labels: ['value', 'SORN'],
+          xLabelAngle: 60,
+          resize: true
+        });
 
-var y = d3.scale.linear().range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-    // .tickFormat(d3.time.format("%Y-%m"));
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-d3.select("#timechart").html('');
-var svg = d3.select("#timechart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// d3.tsv("data.tsv", function(error, data) {
-//   if (error) throw error;
-if(!topic)
-	return;
-console.log(monthD[topic]);
-data = [];
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
-for(var i=0; i<monthNames.length; i++)
-{
-	var r = {};
-	// r.date = parseDate(m)
-	r.date = monthNames[i - 1];
-	r.value = monthD[topic][i];
-	if(r.value == undefined)
-	{
-		r.value = 0;
-	}
-	data.push(r);
-}
-
-for(var m in monthD[topic])
-{
-	var r = {};
-	// r.date = parseDate(m)
-	r.date = monthNames[m - 1];
-	r.value = monthD[topic][m];
-	data.push(r);
-}
-
-// data.forEach(function(d) {
-// d.date = parseDate(d.date);
-// d.close = +d.close;
-// });
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("# of posts");
-
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.date); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
+        $MENU_TOGGLE.on('click', function() {
+          $(window).resize();
+        });
 
 
-// });
 
+        // draw hour
+  
+        d3.select("#hourChart").html('');
+        
+        if(!topic)
+          return;
+
+        data = [];
+        for(var i=1; i<25; i++)
+        {
+           var r = {};
+           // r.date = parseDate(m)
+           r.period = i;
+           r.value = hourData[topic][i];
+           if(r.value == undefined)
+           {
+             r.value = 0;
+           }
+           data.push(r);
+        }
+
+        Morris.Bar({
+          element: 'hourChart',
+          data,
+          xkey: 'period',
+          hideHover: 'auto',
+          barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+          ykeys: ['value', 'sorned'],
+          labels: ['value', 'SORN'],
+          xLabelAngle: 60,
+          resize: true
+        });
+
+        $MENU_TOGGLE.on('click', function() {
+          $(window).resize();
+        });
+
+      });
 }
